6d43ceb321c46c1c22aacb3daf74ee3dca073d94
abhi142s <abhinandan96is@gmail.com>
2025-05-06 08:29:39 +0000

    Create launch file

--- build/arduinobot_description/cmake_install.cmake
+++ build/arduinobot_description/cmake_install.cmake
@@ -46,6 +46,8 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/arduinobot_description" TYPE DIRECTORY FILES
     "/home/abhinandan/ROS2-projects/manipulation/src/arduinobot_description/meshes"
     "/home/abhinandan/ROS2-projects/manipulation/src/arduinobot_description/urdf"
+    "/home/abhinandan/ROS2-projects/manipulation/src/arduinobot_description/launch"
+    "/home/abhinandan/ROS2-projects/manipulation/src/arduinobot_description/rviz"
     )
 endif()
 
